56ccc53b6d17258f4f67b6eac705309976468c84
Alien089 <gabri.moratelli@gmail.com>
2025-03-01 15:00:29 +0100

    initial unreal project

---
+++ FishyBusiness/Source/FishyBusiness/FishyBusinessGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FishyBusinessGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FISHYBUSINESS_API AFishyBusinessGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
