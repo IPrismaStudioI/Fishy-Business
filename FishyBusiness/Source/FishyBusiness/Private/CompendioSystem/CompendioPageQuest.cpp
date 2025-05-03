efd006c50cb00ba520e93cd81ebb55522c9c14e9
Alien089 <gabri.moratelli@gmail.com>
2025-05-03 16:58:46 +0200

    added base quest compendio page (missing ui quest tasks)

--- FishyBusiness/Source/FishyBusiness/Private/CompendioSystem/CompendioPageQuest.cpp
+++ FishyBusiness/Source/FishyBusiness/Private/CompendioSystem/CompendioPageQuest.cpp
@@ -3,6 +3,10 @@
 
 #include "CompendioSystem/CompendioPageQuest.h"
 
+#include "FishyBusiness/FishyBusinessGameModeBase.h"
+#include "Kismet/GameplayStatics.h"
+#include "PlayerSystem/PlayerCharacter.h"
+
 void UCompendioPageQuest::NativeConstruct()
 {
 	Super::NativeConstruct();
@@ -16,4 +20,33 @@ void UCompendioPageQuest::NativeDestruct()
 void UCompendioPageQuest::FillInformations(bool isCatalogued)
 {
 	Super::FillInformations(isCatalogued);
+
+	_xPageImage->SetBrushFromTexture(_xPageTexture);
+}
+
+void UCompendioPageQuest::FillQuest()
+{
+	UAC_QuestLog* log = Cast<APlayerCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(),0))->xQuestLog;
+
+	AFishyBusinessGameModeBase* gamemode = GetWorld()->GetAuthGameMode<AFishyBusinessGameModeBase>();
+	
+	if (log->xQuests.Find(_sQuestID))
+	{
+		_xQuestCanvas->SetVisibility(ESlateVisibility::Visible);
+		FString status = UEnum::GetDisplayValueAsText(log->xQuests[_sQuestID].eStatus).ToString();
+		_xQuestStatus->SetText(FText::FromString(status));
+		FString name = gamemode->xQuestDataManager->GetQuestNameFromDT(_sQuestID);
+		_xQuestName->SetText(FText::FromString(name));
+		FString description = gamemode->xQuestDataManager->GetQuestDescriptionFromDT(_sQuestID);
+		_xQuestDescription->SetText(FText::FromString(description));
+
+		for (int i = 0; i < log->xQuests.Find(_sQuestID)->iCurrentModule; i++)
+		{
+			//FString desc = log->xQuests.Find(_sQuestID)->xModules[i]
+		}
+	}
+	else
+	{
+		_xQuestCanvas->SetVisibility(ESlateVisibility::Collapsed);
+	}
 }
---
+++ FishyBusiness/Source/FishyBusiness/Private/CompendioSystem/QuestModuleUI.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CompendioSystem/QuestModuleUI.h"
+
+void UQuestModuleUI::NativeConstruct()
+{
+	Super::NativeConstruct();
+}
