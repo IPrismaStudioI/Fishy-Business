261caf3b986b417ed3275eae3770c0b0f1d513fd
Arthley <arthleyfamorca@gmail.com>
2025-05-07 19:59:32 +0200

    Added Building Controller, BUG

--- FishyBusiness/Source/FishyBusiness/Private/QuestSystem/QuestBoard/QuestBulletinFocusedUI.cpp
+++ FishyBusiness/Source/FishyBusiness/Private/QuestSystem/QuestBoard/QuestBulletinFocusedUI.cpp
@@ -52,9 +52,11 @@ void UQuestBulletinFocusedUI::FillBulletin(EventParameters parameters)
 void UQuestBulletinFocusedUI::ShowBulletin(bool isShowed)
 {
 	if (isShowed)
+	{
 		_xCanvas->SetVisibility(ESlateVisibility::Visible);
 		OnVisible();
-	else
+	}
+	else 
 		_xCanvas->SetVisibility(ESlateVisibility::Collapsed);
 }
 
